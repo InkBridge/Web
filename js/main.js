1e8841b20fc99ffef591e9d2a666cb60694b6062
raiedkhan <42418263+raiedkhan@users.noreply.github.com>
2018-12-03 23:02:01 +0530

    updated js

--- js/main.js
+++ js/main.js
@@ -18,14 +18,14 @@ function alterflag() {
 function fillcheck() {
     var password = document.getElementById("password").value;
     var password1 = document.getElementById("password1").value;
-    var username = document.getElementById("username").value;
+    var fullname = document.getElementById("fullname").value;
     var penname = document.getElementById("penname").value;
     var email = document.getElementById("email").value;
     var f = document.getElementById("next");
     if (flag == true)
         f.style.display = 'none';
     else {
-        if (password.length != 0 && password1.length != 0 && username.length != 0 && penname.length != 0 &&
+        if (password.length != 0 && password1.length != 0 && fullname.length != 0 && penname.length != 0 &&
             email.length !=
             0) {
             f.style.display = 'block';
@@ -71,14 +71,14 @@ function enabler() {
     var check = document.getElementById("regoption");
     console.log(check);
     if (check.checked) {
-        for (i = 1; i <= 3; i++) {
+        for (i = 1; i <= 5; i++) {
             var subele = document.getElementById(i.toString());
             console.log(i.toString());
             subele.disabled = false;
         }
     }
     if (!(check.checked)) {
-        for (i = 1; i <= 3; i++) {
+        for (i = 1; i <= 5; i++) {
             var subele = document.getElementById(i.toString());
             subele.disabled = true;
         }
