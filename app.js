7b43d3746ece1a8e4db00a79b04f7690087dbcb9
Anmol Duggal <35702521+anmolduggal@users.noreply.github.com>
2018-12-04 19:01:12 +0530

    Routing

---
+++ app.js
@@ -0,0 +1,18 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+ app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); 
+app.use(express.static(__dirname + "/public"));
+app.use(require("express-session")({
+    secret: "Made by InkBridge WebTeam",
+    resave: false,
+    saveUninitialized: false
+}));
+app.get("/",(req,res)=>{
+	res.render("home");
+});
+app.listen(3000,function(){
+    console.log("runnninng");
+});
