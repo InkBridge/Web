37dd8846b90bb7649e85b065d193bc7082bd31fe
Abhinandan-Purkait <32007083+Abhinandan-Purkait@users.noreply.github.com>
2018-12-12 23:46:57 +0530

    Major Change : Notification UI Complete : Comments Focus Complete

--- public/js/homenew.js
+++ public/js/homenew.js
@@ -8,13 +8,7 @@ if (width <= 992) {
     notifbutton.style.display = 'none';
 }
 
-function shownotif() {
-    var prewidth = screen.width;
-    if (prewidth <= 992) {
-        var notifshow = document.getElementById("notif-top");
-        if(notifshow.style.display == 'block')
-            notifshow.style.display = 'none';
-        else
-            notifshow.style.display = 'block'
-    }
+function addfocus(clicked_id)
+{
+    document.getElementById(clicked_id+"F").focus();
 }
