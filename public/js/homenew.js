95792eb15f246ded0fcbac647d020f6347cf421d
Abhinandan-Purkait <32007083+Abhinandan-Purkait@users.noreply.github.com>
2018-12-14 00:36:52 +0530

    notification update

--- public/js/homenew.js
+++ public/js/homenew.js
@@ -1,13 +1,5 @@
 var width = screen.width;
 
-if (width <= 992) {
-    var notifbutton = document.getElementById("notification");
-    notifbutton.style.display = 'block';
-} else {
-    var notifbutton = document.getElementById("notification");
-    notifbutton.style.display = 'none';
-}
-
 function addfocus(clicked_id)
 {
     document.getElementById(clicked_id+"F").focus();
