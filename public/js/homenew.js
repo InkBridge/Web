86a599f5f683314251dbd581fedae38064766ff3
Abhinandan-Purkait <32007083+Abhinandan-Purkait@users.noreply.github.com>
2019-01-17 00:14:36 +0530

    onclick hide and show comment section added

--- public/js/homenew.js
+++ public/js/homenew.js
@@ -2,5 +2,10 @@ var width = screen.width;
 
 function addfocus(clicked_id)
 {
+    var e=document.getElementById(clicked_id+"B");
+    if(e.style.display == 'none')
+        e.style.display = 'block';
+    else if(e.style.display == 'block')
+        e.style.display = 'none';
     document.getElementById(clicked_id+"F").focus();
 }
