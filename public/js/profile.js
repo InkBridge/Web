d5e558e9c67812d1904f60a7b964cc09e5f137e4
Neelanjan Manna <neelanjanmanna@gmail.com>
2019-02-06 16:32:13 +0530

    profile.js updated

--- public/js/profile.js
+++ public/js/profile.js
@@ -37,3 +37,13 @@ function Switch(op,e)
 		drafts.style.display = "block";
 	}
 }
+
+hover_out(){
+	document.getElementById('tags1').style.display = 'none';
+	document.getElementById('content1').style.opacity = '1';
+}
+
+hover_in(){
+	document.getElementById('tags1').style.display = 'block';
+	document.getElementById('content1').style.opacity = '0.4';
+}
