ad23c5fcee6d526d5cdee708c1441b0c7f4383ac
Abhinandan-Purkait <32007083+Abhinandan-Purkait@users.noreply.github.com>
2019-02-08 21:07:48 +0530

    Minor Fixes

--- public/js/homenew.js
+++ public/js/homenew.js
@@ -1,14 +1,25 @@
 var width = screen.width;
 
-function addfocus(clicked_id)
+var comments = document.querySelectorAll("#comment");
+for(var i = 0;i< sqaures.length;i++)
 {
-    var e=document.getElementById(clicked_id+"B");
-    if(e.style.display == 'none')
-        e.style.display = 'block';
-    else if(e.style.display == 'block')
-        e.style.display = 'none';
-    document.getElementById(clicked_id+"F").focus();
+    //add click listeners to squares
+    sqaures[i].addEventListener("click",function(){
+        //grab color of the clicked sqaure 
+        var clickedColor = this.style.background;
+
+        //compare color to pickedColor
+        if(clickedColor === pickedColor){
+            messageDisplay.textContent="Correct";
+        }
+        else
+        {   
+            this.style.background="#232323";
+            messageDisplay.textContent="Try Again";
+        }
+    });
 }
+
 function remove(n)
 {
 	var elem = document.getElementById('consign'+n);
